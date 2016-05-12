101ff082c488a63c93b8fdf9b832560445f0e180
Pradeep Gupta <pradeep.gupta@grocermax.com>
2016-05-12 12:41:59 +0000

    - updated config for latest build and version code.

--- gmxstore/gmxstore/defaults.py
+++ gmxstore/gmxstore/defaults.py
@@ -9,58 +9,79 @@ PACKAGE_DETAILS = {
     "CrateManagement": {
         "id": "1",
         "name": "Crate Management",
-        "package": "com.sakshay.cratemanagement.activity",
-        "version_code": "1",
+        "package": "com.rgretail.cratemanagement.activity",
+        "version_code": "2",
         "icon": "crate.png"
     },
     "CSAllocation": {
         "id": "2",
         "name": "CS Allocation",
-        "package": "com.sakshay.grocermaxnewapp",
-        "version_code": "1",
+        "package": "com.rgretail.grocermaxnewapp",
+        "version_code": "2",
         "icon": "csallocation.png"
     },
     "CDI": {
         "id": "3",
         "name": "Cross Dock Indent",
-        "package": "com.sakshay.crossdockindent",
+        "package": "com.rgretail.crossdockindent",
         "version_code": "1",
         "icon": "csallocation.png"
     },
     "FTP_Support": {
         "id": "4",
         "name": "FTP Support",
-        "package": "com.sakshay.ftpuchasesupportapp",
+        "package": "com.rgretail.ftpuchasesupportapp",
         "version_code": "1",
         "icon": "csallocation.png"
     },
     "FT_Purchase": {
         "id": "5",
         "name": "FT Purchase",
-        "package": "com.sakshay.grocermaxcrate",
+        "package": "com.rgretail.grocermaxcrate",
         "version_code": "1",
         "icon": "csallocation.png"
     },
     "PAI": {
         "id": "6",
         "name": "Put Away Indent",
-        "package": "com.sakshay.putawaypicklist",
+        "package": "com.rgretail.putawaypicklist",
         "version_code": "1",
         "icon": "csallocation.png"
     },
     "PickList": {
         "id": "7",
         "name": "Pick List",
-        "package": "com.sakshay.picklist",
-        "version_code": "1",
+        "package": "com.rgretail.picklist",
+        "version_code": "4",
         "icon": "csallocation.png"
     },
     "WareHouse_Operation": {
         "id": "8",
         "name": "Warehouse Operation",
-        "package": "com.sakshay.warehouseoperation",
-        "version_code": "1",
+        "package": "com.rgretail.warehouseoperation",
+        "version_code": "2",
         "icon": "csallocation.png"
+    },
+    "Core_Operation": {
+        "id": "9",
+        "name": "Core Operation App",
+        "package": "com.rgretail.coreoperationapp",
+        "version_code": "1",
+        "icon": "coreoperations.png"
+    },
+    "GRN": {
+        "id": "10",
+        "name": "GRN App",
+        "package": "com.rgretail.grn",
+        "version_code": "4",
+        "icon": "grn.png"
+    },
+    "IA_App": {
+        "id": "11",
+        "name": "IA App",
+        "package": "com.rgretail.iaapp",
+        "version_code": "4",
+        "icon": "iaa.png"
     },
 }
 
